d2d4ce8bacae423331fc1e5ebe083df043d1296a
Alfred0 GA <alfred007ga@gmail.com>
2020-09-21 13:26:42 -0500

    first commit

---
+++ app/src/main/java/com/alfred0ga/runnerapp/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.alfred0ga.runnerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
